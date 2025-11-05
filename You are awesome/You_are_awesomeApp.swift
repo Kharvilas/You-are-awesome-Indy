c8be24d572f434df7d8956343a6428c573a9f5a5
Indy <jaker4814@gmail.com>
2025-11-05 12:42:21 -0800

    Initial Commit

---
+++ You are awesome/You_are_awesomeApp.swift
@@ -0,0 +1,17 @@
+//
+//  You_are_awesomeApp.swift
+//  You are awesome
+//
+//  Created by Student3 on 11/5/25.
+//
+
+import SwiftUI
+
+@main
+struct You_are_awesomeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
